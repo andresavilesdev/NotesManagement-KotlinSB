ee070a778ae7e5c687055b10f0387ccd10487183
Andresavilesdev <andresaviles0721@gmail.com>
2025-06-03 00:53:49 -0500

    saving notes to mongodb with error about spring security

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
-	implementation("org.springframework.boot:spring-boot-starter-security")
+//	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.security:spring-security-crypto")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
--- src/main/kotlin/com/kotlin/course/poject/SavingOfNotes/note/controller/NoteController.kt
+++ src/main/kotlin/com/kotlin/course/poject/SavingOfNotes/note/controller/NoteController.kt
@@ -1,4 +1,70 @@
 package com.kotlin.course.poject.SavingOfNotes.note.controller
 
-class NoteController {
+import com.kotlin.course.poject.SavingOfNotes.note.model.Note
+import com.kotlin.course.poject.SavingOfNotes.note.repository.INoteRepository
+import org.bson.types.ObjectId
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.time.Instant
+
+// https://notes.com/notes/...
+
+@RestController
+@RequestMapping("/notes")
+class NoteController (private val repository: INoteRepository) {
+
+
+    data class NoteRequest(
+        val id: String?,
+        val title: String,
+        val content: String,
+        val color: Long,
+        val ownerId: String?
+    )
+    data class NoteResponse(
+        val id: String,
+        val title: String,
+        val content: String,
+        val color: Long,
+        val createdAt: Instant
+    )
+
+    @PostMapping()
+    fun save(body: NoteRequest): NoteResponse {
+        val note = repository.save(
+            Note(
+                id = body.id?.let { ObjectId(it) } ?: ObjectId.get(),
+                title = body.title,
+                content = body.content,
+                color = body.color,
+                createdAt = Instant.now(),
+                ownerId = ObjectId(body.ownerId)
+
+            )
+        )
+
+        return note.toResponse()
+
+    }
+
+    @GetMapping("/{id}")
+    fun findByOwnerId(@RequestParam(required = true) ownerId: String): List<NoteResponse> {
+        return repository.findByOwnerId(ObjectId(ownerId)).map {
+                it.toResponse()
+
+        }
+    }
+
+    private fun Note.toResponse(): NoteController.NoteResponse{
+        return NoteResponse(
+            id = id.toHexString(),
+            title = title,
+            content = content,
+            color = color,
+            createdAt = createdAt,
+        )
+    }
 }
